70bba1e821ab42b37faa45e61c09f990114d5281
HarshadYadav <harshad.y@syborgtech.com>
2025-01-03 15:16:37 +0530

    Form changes

--- manage-master/src/Pages/AddVillgae.jsx
+++ manage-master/src/Pages/AddVillgae.jsx
@@ -15,12 +15,19 @@ export const AddVillage = () => {
   const [loadingBlock, setLoadingBlock] = useState(false);
   const [loadingPincode, setLoadingPincode] = useState(false);
 
+  const liveMarketOptions = [
+    { id: "true", name: "Yes" },
+    { id: "false", name: "No" },
+  ];
   const [data, setData] = useState({
+    village: "",
     stateId: "",
     districtId: "",
     subDistrictId: "",
     blockId: "",
     pincode: "",
+    isLiveInMarket: "Yes",
+    liveForDelivery: "Yes",
   });
 
   const fetchData = async (url, setter, setLoading) => {
@@ -73,6 +80,14 @@ export const AddVillage = () => {
       (data) => setPincodeData(data.pinCodes),
       setLoadingPincode
     );
+  };
+
+  const handelchange = (e) => {
+    const { name, value } = e.target;
+    setData((prev) => ({
+      ...prev,
+      [name]: value === "true" ? true : value === "false" ? false : value,
+    }));
   };
 
   const handleStateChange = (e) => {
@@ -130,6 +145,19 @@ export const AddVillage = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    if (
+      !data.village ||
+      !data.stateId ||
+      !data.districtId ||
+      !data.subDistrictId ||
+      !data.blockId ||
+      !data.pincode
+    ) {
+      alert("All fields are mandatory. Please fill in all the details.");
+      return;
+    }
+
     const selectedState = stateData.find(
       (state) => state.stateId === data.stateId
     );
@@ -141,6 +169,7 @@ export const AddVillage = () => {
     );
 
     const payload = {
+      village: data.village,
       stateId: data.stateId,
       districtId: data.districtId,
       subDistrictId: data.subDistrictId,
@@ -152,90 +181,145 @@ export const AddVillage = () => {
     };
 
     console.log("Form data submitted:", payload);
+
+    setData({
+      stateId: "",
+      districtId: "",
+      subDistrictId: "",
+      blockId: "",
+      pincode: "",
+      village: "",
+      isLiveInMarket: false,
+      isLiveForDelivery: false,
+    });
   };
 
   return (
     <div className="flex flex-col">
-      <h1>Add Village</h1>
-      <form onSubmit={handleSubmit}>
-        <div className="flex justify-between flex-wrap gap-4">
-          <div className="flex-1 min-w-[150px]">
-            <label className="block mb-1">Village</label>
-            <input
-              type="text"
-              placeholder="Enter Village Name"
-              className="block w-full p-2"
-            />
+      <div className="p-4 bg-white m-2 rounded-lg shadow-lg  w-full box-border">
+        <h1 className="text-xl font-semibold mb-4">Add Village</h1>
+        <form onSubmit={handleSubmit}>
+          <div className="flex flex-wrap gap-4">
+            <div className="flex-1 min-w-[150px]">
+              <label className="block">Village</label>
+              <input
+                type="text"
+                placeholder="Enter Village Name"
+                className="block w-full p-2 border border-gray-300 rounded"
+                name="village"
+                value={data.village}
+                onChange={handelchange}
+                required
+              />
+            </div>
+            <div className="flex-1 min-w-[150px]">
+              <SelectInput
+                label="State"
+                name="stateId"
+                value={data.stateId}
+                onChange={handleStateChange}
+                options={stateData.map((state) => ({
+                  id: state.stateId,
+                  name: state.state,
+                }))}
+                loading={loadingStates}
+                required
+              />
+            </div>
+            <div className="flex-1 min-w-[150px]">
+              <SelectInput
+                label="District"
+                name="districtId"
+                value={data.districtId}
+                onChange={handleDistrictChange}
+                options={districtData.map((district) => ({
+                  id: district.districtId,
+                  name: district.district,
+                }))}
+                loading={loadingDistricts}
+                disabled={loadingDistricts || !districtData.length}
+                required
+              />
+            </div>
           </div>
-          <SelectInput
-            label="State"
-            name="stateId"
-            value={data.stateId}
-            onChange={handleStateChange}
-            options={stateData.map((state) => ({
-              id: state.stateId,
-              name: state.state,
-            }))}
-            loading={loadingStates}
-          />
-          <SelectInput
-            label="District"
-            name="districtId"
-            value={data.districtId}
-            onChange={handleDistrictChange}
-            options={districtData.map((district) => ({
-              id: district.districtId,
-              name: district.district,
-            }))}
-            loading={loadingDistricts}
-            disabled={loadingDistricts || !districtData.length}
-          />
-        </div>
-        <div className="flex justify-between flex-wrap gap-4">
-          <SelectInput
-            label="SubDistrict"
-            name="subDistrictId"
-            value={data.subDistrictId}
-            onChange={handleSubDistrictChange}
-            options={subDistrictData.map((subDistrict) => ({
-              id: subDistrict.subDistrictId,
-              name: subDistrict.subDistrict,
-            }))}
-            loading={loadingSubdistrict}
-            disabled={loadingSubdistrict || !subDistrictData.length}
-          />
-          <SelectInput
-            label="Block"
-            name="blockId"
-            value={data.blockId}
-            onChange={handleBlockChange}
-            options={blockData.map((block) => ({
-              id: block.blockId,
-              name: block.block,
-            }))}
-            loading={loadingBlock}
-            disabled={loadingBlock || !blockData.length}
-          />
-          <SelectInput
-            label="Pincode"
-            name="pincode"
-            value={data.pincode}
-            onChange={handlePincodeChange}
-            options={pincodeData.map((pincode) => ({
-              id: pincode.pincode,
-              name: pincode.pinCode,
-            }))}
-            loading={loadingPincode}
-            disabled={loadingPincode || !pincodeData.length}
-          />
-        </div>
-        <button
-          type="submit"
-          className="mt-4 p-2 bg-blue-500 text-white rounded"
-        >
-          Submit
-        </button>
-      </form>
+          <div className="flex flex-wrap gap-4 mt-4">
+            <div className="flex-1 min-w-[150px]">
+              <SelectInput
+                label="SubDistrict"
+                name="subDistrictId"
+                value={data.subDistrictId}
+                onChange={handleSubDistrictChange}
+                options={subDistrictData.map((subDistrict) => ({
+                  id: subDistrict.subDistrictId,
+                  name: subDistrict.subDistrict,
+                }))}
+                loading={loadingSubdistrict}
+                disabled={loadingSubdistrict || !subDistrictData.length}
+                required
+              />
+            </div>
+            <div className="flex-1 min-w-[150px]">
+              <SelectInput
+                label="Block"
+                name="blockId"
+                value={data.blockId}
+                onChange={handleBlockChange}
+                options={blockData.map((block) => ({
+                  id: block.blockId,
+                  name: block.block,
+                }))}
+                loading={loadingBlock}
+                disabled={loadingBlock || !blockData.length}
+                required
+              />
+            </div>
+            <div className="flex-1 min-w-[150px]">
+              <SelectInput
+                label="Pincode"
+                name="pincode"
+                value={data.pincode}
+                onChange={handlePincodeChange}
+                options={pincodeData.map((pincode) => ({
+                  id: pincode.pincode,
+                  name: pincode.pinCode,
+                }))}
+                loading={loadingPincode}
+                disabled={loadingPincode || !pincodeData.length}
+                required
+              />
+            </div>
+          </div>
+          <div className="flex flex-wrap gap-4 mt-4">
+            <div className="flex-1 min-w-[150px]">
+              <SelectInput
+                label="Is Live In Market"
+                name="isLiveInMarket"
+                value={data.isLiveInMarket}
+                onChange={handelchange}
+                options={liveMarketOptions}
+                required
+              />
+            </div>
+            <div className="flex-1 min-w-[150px]">
+              <SelectInput
+                label="Live For Delivery"
+                name="liveForDelivery"
+                value={data.liveForDelivery}
+                onChange={handelchange}
+                options={liveMarketOptions}
+                required
+              />
+            </div>{" "}
+            ;
+          </div>
+          <button
+            type="submit"
+            className="mt-4 p-2 bg-blue-500 text-white rounded w-full sm:w-auto"
+          >
+            Submit
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
