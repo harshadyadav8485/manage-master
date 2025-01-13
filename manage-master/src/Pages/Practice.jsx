a97b0bc315174ab9542777601495bdb8ce93a638
HarshadYadav <harshad.y@syborgtech.com>
2025-01-13 14:53:46 +0530

    Manage_master changes 1

--- manage-master/src/Pages/Practice.jsx
+++ manage-master/src/Pages/Practice.jsx
@@ -5,6 +5,8 @@ const Practice = () => {
       <h1>Stage 1 changes</h1>
       <h1>Stage 2 changes</h1>
       <h1>Stage 3 changes</h1>
+      <h1>Stage 4 changes</h1>
+      <h1>Stage 5</h1>
     </>
   );
 };
